6eec82f9560856a1044319208ddc65deecbd972d
qikcik <qikcik@gmail.com>
2019-08-11 19:32:23 +0200

    added syncing unconfirmed order and split order id witch mogdob orders id. phone number validator don't done yet

--- baselinker.js
+++ baselinker.js
@@ -19,42 +19,68 @@ async function Request(method, parameters, key) {
     });
 }
 
+function MapBaselinkerOrderToSimplified(order) {
+    return {
+        _id: order.order_id,
+        phone: order.phone,
+        confirmed: order.confirmed
+    }
+}
+
+async function AddNewUnconfirmedOrders(baselinker) {
+    try {
+        var lastId = await baselinker.db.collection('orders').find().sort({ _id: -1 }).limit(1).toArray();
+        if (lastId.length == 0)
+            lastId = 0;
+        else
+            lastId = lastId[0]._id + 1;
+
+        var data = await Request("getOrders", {
+            id_from: lastId,
+            get_unconfirmed_orders: true
+        }, baselinker.key);
+
+        data = JSON.parse(data);
+
+        if (data.status != "SUCCESS")
+            throw new Error(`bad query status ${data}`);
+
+
+        // Push Data
+
+        // mapping data
+        var mapped = [];
+
+        for (var i in data.orders) {
+            mapped.push(MapBaselinkerOrderToSimplified(data.orders[i]));
+        }
+
+        var result = await baselinker.db.collection('orders').insertMany(mapped);
+
+        console.log(`insert ${mapped.length} witch last id ${mapped[mapped.length-1]._id}`);
+
+        if (data.orders.length == 100)
+            return AddNewUnconfirmedOrders(baselinker);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    // success
+    return Promise.resolve(true);
+}
+
 
 module.exports = class BaseLinker {
     constructor(MongoDB, key) {
         this.db = MongoDB;
         this.key = key;
+        this.isSyncing = false;
     }
 
-    async getNewOrders() {
-
-        try {
-            let lastId = await this.db.collection('orders').find().sort({ id: -1 }).limit(1).toArray();
-            if (lastId.length = 0)
-                lastId = 0;
-            else
-                lastId = lastId[0]++;
-
-            var data = await Request("getOrders", {
-                id_from: lastId,
-                get_unconfirmed_orders: true
-            }, this.key);
-
-            data = JSON.parse(data);
-
-            if (data.status != "SUCCESS")
-                throw new Error(`bad query status ${data}`);
-
-            console.log(data);
-            // Push Data
-
-            if (data.orders.length == 100)
-                return this.getNewOrders();
-
-        } catch (err) {
-            console.log(err);
-        }
-
-        return Promise.resolve(":)");
+    async sync() {
+        this.isSyncing = true;
+        await AddNewUnconfirmedOrders(this);
+        this.isSyncing = false;
     }
 };
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var Database = require('./database.js');
 var Baselinker = require('./baselinker.js');
 
 var database = new Database();
-database.connect(`mongodb://localhost:27057`, `debug`)
+database.connect(`mongodb://localhost:27057`, `debug2`)
     .then(db => {
         var baselinker = new Baselinker(db, '1652-1186-ZRFTDJMXJG48Q06F5FSOUWP4CRY59HB3M10LOQ2C8QX9FF8J0NYCA57RNTEW0355');
-        baselinker.getNewOrders().then(() => console.log("succes"));
+        baselinker.sync().then(() => console.log("succes"));
 
         //console.log(baselinker);
     })
